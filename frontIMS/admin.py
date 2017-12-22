062c67fb66dd3e683563f69a50a3dc56874e6fba
崔杰 <1904672597@qq.com>
2017-12-22 22:24:03 +0800

    建数据库

--- frontIMS/admin.py
+++ frontIMS/admin.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+# register your models here.
+import models
+
+admin.site.register(models.Student)
+admin.site.register(models.Institude)
+admin.site.register(models.Teacher)
+admin.site.register(models.Couser)
+admin.site.register(models.Major)
+admin.site.register(models.Grade)
+admin.site.register(models.Score)
+
--- frontIMS/models.py
+++ frontIMS/models.py
@@ -4,61 +4,31 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Admin(models.Model):
+
+
+class Institude(models.Model):
     '''
-    管理员表， 用作后台管理
+    学院表
     '''
-    Id = models.AutoField(primary_key=True)
-    Account = models.CharField(null=False, blank=False, unique=True, max_length=25)
-    Password = models.CharField(null=False, blank=False, max_length=25)
-    DateTime = models.DateTimeField(auto_now_add=True)
+    InstitudeId = models.CharField(primary_key=True, max_length=20)
+    InstitudeName = models.CharField(null=False, max_length=20)
 
-
-class User(models.Model):
-    '''
-    1.身份标识
-        0学生
-        1教师
-    2.性别标识
-        0男生
-        1女生
-    '''
-    Id = models.AutoField(primary_key=True)
-    Identity = models.PositiveSmallIntegerField(default=0, null=False)
-    PassWord = models.CharField(max_length=20, null=False)
-    Sex = models.PositiveSmallIntegerField(default=0, null=False)
-    UserName = models.CharField(max_length=10, null=False)
-    Institude = models.CharField(null=True, max_length=20)
-
-
-
-
-class Student(models.Model):
-    '''
-    学生表
-    '''
-    Id = models.AutoField(primary_key=True)
-    Grade = models.CharField(null=False, max_length=20)
-    Major = models.CharField(null=True, max_length=20)
-
+    def __unicode__(self):
+        return self.InstitudeName
 
 
 class Teacher(models.Model):
     '''
     教师表
     '''
-    Id = models.AutoField(primary_key=True)
-    cNO = models.CharField(null=False, max_length=20)
+    TeacherId = models.CharField(primary_key=True, max_length=20)
+    PassWord = models.CharField(max_length=20, null=False)
+    UserName = models.CharField(max_length=10, null=False)
+    InstitudeId = models.ForeignKey(Institude, max_length=20)
 
+    def __unicode__(self):
+        return self.UserName
 
-
-class Score(models.Model):
-    '''
-    成绩表
-    '''
-    Id = models.AutoField(primary_key=True)
-    Score = models.CharField(null=False, max_length=20)
-    cNO = models.CharField(null=False, max_length=20)
 
 
 class Couser(models.Model):
@@ -66,8 +36,66 @@ class Couser(models.Model):
     课程表
     '''
 
-    cNO = models.CharField(primary_key=True, max_length=20)
-    CouserName = models.CharField(null=True, max_length=20)
-    Credit = models.CharField(null=True, max_length=20)
+    CouserId = models.CharField(primary_key=True, max_length=20)
+    CouserName = models.CharField(null=False, max_length=20)
     Period = models.CharField(null=False, max_length=20 )
-    
+    TeacherId = models.ForeignKey(Teacher, max_length=20)
+
+    def __unicode__(self):
+        return self.CouserName
+
+
+
+class Major (models.Model):
+    '''
+    专业表
+    '''
+    MajorId = models.CharField(primary_key=True, max_length=20)
+    MajorName = models.CharField(null=False, max_length=20)
+    InstitudeId = models.ForeignKey(Institude, max_length=20)
+
+    def __unicode__(self):
+        return self.MajorName
+
+
+class Grade (models.Model):
+    '''
+    班级表
+    '''
+    GradeId = models.CharField(primary_key=True, max_length=20)
+    GradeName = models.CharField(null=False, max_length=20)
+    GradeNum = models.CharField(null=False, max_length=20)
+    MajorId = models.ForeignKey(Major, max_length=20)
+    InstitudeId = models.ForeignKey(Institude, max_length=20)
+
+    def __unicode__(self):
+        return self.GradeName
+
+
+class Student(models.Model):
+    '''
+    学生表
+    '''
+    StudentId = models.CharField(primary_key=True, max_length=20)
+    PassWord = models.CharField(max_length=20, null=False)
+    UserName = models.CharField(max_length=10, null=False)
+    StuTeacher = models.ManyToManyField(Couser)
+    GradeId = models.ForeignKey(Grade, max_length=20)
+    MajorId = models.ForeignKey(Major, max_length=20)
+    InstitudeId = models.ForeignKey(Institude, max_length=20)
+
+    def __unicode__(self):
+        return self.StudentId
+
+class Score(models.Model):
+    '''
+    成绩表
+    '''
+    ScoreId = models.CharField(primary_key=True, max_length=20)
+    Score = models.CharField(null=False, max_length=20)
+    CouserId = models.ForeignKey(Couser, max_length=20)
+    StudentId = models.ForeignKey(Student, max_length=20)
+
+    def __unicode__(self):
+        return self.ScoreId
+
