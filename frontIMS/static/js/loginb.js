9f71756f6cafc689e4ec9b083440817c40941568
cuidaguoguo <19046725972@qq.com>
2017-12-23 17:51:06 +0800

    登录

--- frontIMS/static/js/loginb.js
+++ frontIMS/static/js/loginb.js
@@ -5,16 +5,32 @@
 // $(function () {
 //     var account = $("input[id='account']").val("");
 //     var password = $("input[id='password']").val("");
+//
 // });
 
 
 //判断信息是否填写完成
 function login() {
-    var password = document.getElementById("password").value;
-    var account = document.getElementById("account").value;
+    var account = $("input[id='account']").val();
+    var password = $("input[id='password']").val();
     if(password === null || password === undefined || password === ''){
-        alert("请输入账号")
+        alert("请输入密码")
     }else if(account === null || account === undefined || account === ''){
         alert("请输入账号")
+    }else{
+        $.post("loginb",{password:password,account:account},function (result) {
+            result = JSON.parse(result);
+                if (result["status"] === 0) {
+                    alert(result["message"]);
+                    window.location.reload();
+                } else if (result["status"] === 1) {
+                    var username =  $.cookie('username', result["username"]);
+                    var teacherid = $.cookie('teacherid',result["teacherid"]);
+                    window.location.href =  '/frontIMS/index'
+                } else {
+                    alert('服务器异常');
+                    window.location.reload()
+                }
+        })
     }
 }
--- frontIMS/views.py
+++ frontIMS/views.py
@@ -1,26 +1,78 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# from _mysql import result
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+import uuid
+import re, base64
+from django.utils.html import escapejs
+
+from frontIMS import models
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, HttpResponse, render_to_response, get_object_or_404, Http404
 from django.db.models import Q
-
+import json
+import time
+import time
+from frontIMS.models import Institude, Teacher, Couser, Major, Grade, Student, Score
 
 # Create your views here.
 
 
-
+@csrf_exempt
 def loginb(req):
     if req.method == "GET":
         return render_to_response('loginb.html')
     if req.method == "POST":
-        pass
+        try:
+            account = req.POST['account']
+            password = req.POST['password']
+        except Exception as e:
+            result = {
+                'status': 0,
+                'message': "获取信息失败"
+            }
+            return HttpResponse(json.dumps(result))
+        else:
+            if models.Teacher.objects.filter(Q(TeacherId=account)):
+                user = models.Teacher.objects.get(TeacherId=account)
+                if user.PassWord == password:
+                    result = {
+                        'status': 1,
+                        'message': "登录成功",
+                        'username':user.UserName,
+                        'teacherid':user.TeacherId
+                    }
+
+                    return HttpResponse(json.dumps(result))
+                elif user.PassWord != password:
+                    result = {
+                        'status': 0,
+                        'message': "用户密码不正确",
+                    }
+                    return HttpResponse(json.dumps(result))
+            else:
+                result = {
+                    'status': 0,
+                    'message': "用户密码不正确",
+                }
+                return HttpResponse(json.dumps(result))
+            
 
 
-
+@csrf_exempt
 def index(req):
     if req.method == "GET":
-        return render_to_response('index.html')
+        teacherid = req.COOKIES.get('teacherid')
+        username = req.COOKIES.get('username')
+        teacher = models.Teacher.objects.all()
+        return render_to_response('index.html',{"teacher":teacher})
     if req.method == "POST":
         pass
 
